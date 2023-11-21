cdb174bfc0848dcbec73746844c2ac6746d3d00b
dyharlan <dyharlan45@gmail.com>
2023-11-21 19:26:38 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# IS2-LabExercise3
+ Hair Type Classifier using Convolutional Neural Networks
---
+++ setup.sh
@@ -0,0 +1,1 @@
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple tensorflow[and-cuda]==2.14 numpy matplotlib pydot graphviz pandas
